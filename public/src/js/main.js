3267b083d4425a2f126446fd8e867e986535a15d
Joseph Billingsley <jb@josephbillingsley.co.uk>
2017-01-03 23:14:54 +0000

    First push to live

--- public/src/js/main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("resize", function(e) {
-    var resize = document.getElementsByClassName("resize");
-    
-    for (var i = 0; i < resize.length; i++) {
-        resize[i].style.width = resize[i].offsetHeight * .2;
-    }
-});
